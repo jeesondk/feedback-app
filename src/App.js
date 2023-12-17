a6957edfa6347818977ec561065c37dbed01ea7f
Jesper Jensen <jesper.jensen@keemail.me>
2023-12-17 14:30:17 +0100

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import {v4 as uuidv4} from "uuid"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackData from "./data/feedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from "./components/AboutIconLink"
+import AboutPage from "./pages/AboutPage"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,15 +24,24 @@ function App() {
     }
 
 return (
-        <>
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback = {feedback}
-                handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback = {feedback}
+                            handleDelete={deleteFeedback} />
+                        </>
+                    }></Route>
+
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
     )
 }
 
